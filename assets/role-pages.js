9083e4ee2d0bf4beb42aaf92f18cc984c4be786e
Art-Kustuff <akustov1995@gmail.com>
2026-05-14 14:42:59 +0300

    Add files via upload

---
+++ assets/role-pages.js
@@ -0,0 +1,7 @@
+const obs = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.add('visible');
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.fade-in').forEach((el) => obs.observe(el));
